e49dac1ca8d8c13d0b48f82cf931b702bed87af7
tisukRoman <tisukroman@gmail.com>
2022-01-07 15:53:28 +0200

    LinkedList updated

--- data_structures/LinkedList.js
+++ data_structures/LinkedList.js
@@ -25,8 +25,108 @@ class LinkedList {
         this.length++;
     };
 
+    addAt(index, element) {
+        const nodeToAdd = new Node(element);
+        let currentIndex = 0;
+        let current = this.head;
+        let previous;
+
+        if (index < 0 || index >= this.length) {
+            return false;
+        }
+
+        if (index === 0) {
+            this.head = nodeToAdd;
+            this.head.next = current;
+            this.length++;
+        } else {
+            while (current.next) {
+                if (currentIndex === index) {
+                    previous.next = nodeToAdd;
+                    nodeToAdd.next = current;
+                    this.length++;
+                    return;
+                } else {
+                    previous = current;
+                    current = current.next;
+                    currentIndex++;
+                }
+            }
+        }
+    }
+
     remove(element) {
-        // TODO: this method
+        let current = this.head;
+        let previous;
+
+        if (current.element === element) {
+            this.head = current.next;
+        } else {
+            while (current.element !== element) {
+                previous = current;
+                current = current.next;
+            }
+            previous.next = current.next;
+        }
+        this.length--;
+    }
+
+    removeAt(index) {
+        let currentIndex = 0;
+        let current = this.head;
+        let previous;
+
+        if (currentIndex === index) {
+            this.head = current.next;
+            this.length--;
+            return current.element;
+        }
+
+        while (current.next) {
+            if (currentIndex === index) {
+                previous.next = current.next;
+                this.length--;
+                return current.element;
+            } else {
+                previous = current;
+                current = current.next;
+                currentIndex++;
+            }
+        }
+        return null;
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+
+    indexOf(element) {
+        let index = 0;
+        let current = this.head;
+
+        while (current.next) {
+            if (current.element === element) {
+                return index;
+            } else {
+                current = current.next;
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    elementAt(index) {
+        let current = this.head;
+        let currentIndex = 0;
+        while (current.next) {
+            if (currentIndex === index) {
+                return current.element;
+            } else {
+                current = current.next;
+                currentIndex++;
+            }
+        }
+        return undefined;
     }
 };
 
@@ -36,3 +136,31 @@ class Node {
         this.next = null;
     }
 };
+
+
+/* 
+const head = {
+    element: 'hello',
+    next: {
+        element: 'world',
+        next: {
+            element: 'div',
+            next: {
+                element: 'span',
+                next: null
+            }
+        }
+    }
+} 
+*/
+
+const ll = new LinkedList();
+ll.add('hello');
+ll.add('world');
+ll.add('div');
+ll.add('span');
+
+ll.addAt(1, 'Greek')
+/* console.log(ll.addAt(3));  */
+
+console.log(ll);
